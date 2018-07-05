64d2352f4ad053e018ad36172c9996206346d3d2
CreeperJim <1010660838@qq.com>
2018-07-05 08:36:00 +0800

    assets

---
+++ Scripts/DestroyByContact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject Explosion;
+    public GameObject playerExplosion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Boundary")
+        {
+            return;
+        }
+        Instantiate(Explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+
+    private void Start()
+    {
+        SpawnWaves();
+    }
+
+    void SpawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3(Random. Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z) ;
+        Quaternion spawnRotation = new Quaternion();
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+}
